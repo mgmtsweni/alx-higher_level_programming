1a86abc6596dd3653f763e6c7c7ad851fe74a1f6
Mtsweni Gift <mtswenigift@gmail.com>
2022-07-07 07:07:06 -0700

    Pycodestel update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys 
+    import sys
 
-    count = len(sys.argv) - 1
+count = len(sys.argv) - 1
     if count == 0:
         print("{} arguments.".format(count))
     elif count == 1:
         print("{} arguments.".format(count))
     else:
         print("{} arguments.".format(count))
-
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
