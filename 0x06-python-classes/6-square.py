b80a314d1da4355ab555e0d8aad3f3ade7bf116f
Gift Mtsweni <mtswenigift@gmail.com>
2022-07-20 01:40:52 +0200

    Python Code Style Update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,10 +36,10 @@ class Square:
                 for rows in range(self.__size):
                     print("#" * self.__size)
 
-        def postion(self, value):
-            if (len(value) != 2) or (type(value) is not tuple) \
-                or (type(value[0]) is not int) or (type(value[1]) is not int) \
-                or (value[0] < 0) or (value[1] < 0):
-                raise TypeError("position must be a tuple of 2 positive integers")
-            else:
-                self.__position = value
+    def postion(self, value):
+        if (len(value) != 2) or (type(value) is not tuple) \
+            or (type(value[0]) is not int) or (type(value[1]) is not int) \
+            or (value[0] < 0) or (value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
