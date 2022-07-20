56f3e8ead23a18f0e3e04d613364d065032f4e97
Gift Mtsweni <mtswenigift@gmail.com>
2022-07-20 02:15:45 +0200

    With Advance

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,7 +12,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
 
         def area(self):
             """Returns the area of a Square instance"""
