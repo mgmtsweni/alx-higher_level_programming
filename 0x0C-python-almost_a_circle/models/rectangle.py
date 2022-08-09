5e8530d77c67209dc17e83cd3e8364c3d393be63
mgmtsweni <mtswenigift@gmail.com>
2022-08-09 02:38:57 +0200

    Rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This module implements `rectangle` class
+"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """class Rectangle that inherits from Base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+    
+    @property
+    def width(self):
+        """getter for width"""
+        return self.__width
+    
+    @property
+    def height(self):
+        """getter for height"""
+        return self.__height
+    
+    @property
+    def  x(self):
+        """getter for x"""
+        return self.__x
+    
+    @property
+    def y(self):
+        """getter for y"""
+        return self.__y
