6744e4f22a6c3d7ff550e3ef6228f114615494e1
mgmtsweni <mtswenigift@gmail.com>
2022-08-09 02:15:18 +0200

    Update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,7 +7,7 @@ This module implements `base` class of all other classes
 class Base():
     """implementation"""
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         """Initialize id, increment class attribute if
         no id and set as id
