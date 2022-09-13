73bc73fc8bb5caa9575cd212b40e6fd34c2b53d6
mgmtsweni <mtswenigift@gmail.com>
2022-09-13 18:50:56 +0200

    SQL Update

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,16 @@
+-- creates a table second_table in the database hbtn_0c_0
+CREATE TABLE second_table (
+    id int,
+    name varchar(255),
+    score INT
+);
+
+INSERT INTO `second_table` (
+    `id`,
+    `name`,
+    `score`)
+    VALUES 
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
