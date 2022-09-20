798caf451632ce2b1e07d69987197cbb0716f039
mgmtsweni <mtswenigift@gmail.com>
2022-09-20 14:09:58 +0200

    Update

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- script that lists all records FROM second_table
+SELECT  score, name FROM second_table ORDER BY `score` DESC;
---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- a script that lists the number of records
+
+SELECT score, COUNT(score) AS 'number'
+FROM second_table GROUP BY score ORDER BY 'number' DESC;
