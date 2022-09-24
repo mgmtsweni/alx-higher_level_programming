52073fb28bb5b0dfd9556240a582a1a5c6229d43
mgmtsweni <mtswenigift@gmail.com>
2022-09-24 11:54:28 +0200

    update

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,7 @@
 -- Import the database dump of hbtn_0d_tvshows to your MySQL server
 -- script that lists all non-Comedy shows in the database hbtn_0d_tvshows
 
-FROM tv_shows WHERE tv_shows.title NOT IN (
+SELECT tv_shows.title FROM tv_shows WHERE tv_shows.title NOT IN (
     SELECT tv_shows.title FROM tv_shows
     INNER JOIN tv_show_genres
     ON tv_shows.id = tv_show_genres.show_id
