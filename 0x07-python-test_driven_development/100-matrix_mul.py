0bee79a4349e7060e761231d353f135017367c7f
mgmtsweni <mtswenigift@gmail.com>
2022-09-24 09:12:29 +0200

    update

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -43,11 +43,11 @@ def matrix_mul(m_a, m_b):
         raise ValueError("m_a and m_b can't be multiplied")
     matrix = []
     for i in range(l1):
-        l = []
+        ln = []
         for j in range(l3):
             n = 0
             for k in range(l2):
                 n += m_a[i][k] * m_b[k][j]
-            l.append(n)
-        matrix.append(l)
+            ln.append(n)
+        matrix.append(ln)
     return matrix
