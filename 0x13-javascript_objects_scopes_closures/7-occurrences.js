5a649703c7fee6545710ab3e9747d3b08c77d417
mgmtsweni <mtswenigift@gmail.com>
2022-10-08 08:07:45 +0200

    Update

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  for (var i in list) {
+  for (let i in list) {
     if (list[i] === searchElement) {
       count++;
     }
